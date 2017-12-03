33e16c79fde3f95b1f074f7a157f87226294c5fc
Chris Xue <chrisxue815@gmail.com>
2017-12-03 10:43:18 +0000

    463. Island Perimeter.

--- problem/p463.go
+++ problem/p463.go
@@ -2,26 +2,23 @@ package problem
 
 // O(n) time. O(1) space. Iteration.
 func islandPerimeter(grid [][]int) int {
-	peri := 0
+	islands := 0
+	neighbors := 0
 
 	for r, row := range grid {
 		for c, num := range row {
 			if num == 1 {
-				if r == 0 || grid[r-1][c] == 0 {
-					peri++
+				islands++
+
+				if r >= 1 && grid[r-1][c] == 1 {
+					neighbors++
 				}
-				if r == len(grid)-1 || grid[r+1][c] == 0 {
-					peri++
-				}
-				if c == 0 || row[c-1] == 0 {
-					peri++
-				}
-				if c == len(row)-1 || row[c+1] == 0 {
-					peri++
+				if c >= 1 && row[c-1] == 1 {
+					neighbors++
 				}
 			}
 		}
 	}
 
-	return peri
+	return islands*4 - neighbors*2
 }
