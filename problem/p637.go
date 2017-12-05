6739a0280d84cdb6c9ce4459df3f6affe944db83
Chris Xue <chrisxue815@gmail.com>
2017-12-05 18:35:44 +0000

    637. Average of Levels in Binary Tree.

---
+++ problem/p637.go
@@ -0,0 +1,41 @@
+package problem
+
+import (
+	. "leetcode-go/util"
+)
+
+const initialCapacity = 16
+
+// O(n) time. O(n) space. BFS.
+func averageOfLevels(root *TreeNode) []float64 {
+	if root == nil {
+		return []float64{}
+	}
+
+	result := make([]float64, 0, initialCapacity)
+	queue := make([]*TreeNode, 0, initialCapacity)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		levelLength := len(queue)
+		sum := 0
+
+		for i := 0; i < levelLength; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			sum += node.Val
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		result = append(result, float64(sum)/float64(levelLength))
+	}
+
+	return result
+}
---
+++ problem/p637_test.go
@@ -0,0 +1,21 @@
+package problem
+
+import (
+	"github.com/stretchr/testify/assert"
+	. "leetcode-go/util"
+	"testing"
+)
+
+func Test_averageOfLevels(t *testing.T) {
+	tests := []struct {
+		root     string
+		expected []float64
+	}{
+		{"3,9,20,#,#,15,7", []float64{3, 14.5, 11}},
+	}
+
+	for _, test := range tests {
+		actual := averageOfLevels(Deserialize(test.root))
+		assert.Equal(t, test.expected, actual, "%+v", test)
+	}
+}
