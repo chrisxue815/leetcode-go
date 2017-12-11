ce4ba4eca56204a6f3d80730f20c28cdcba349fe
Chris Xue <chrisxue815@gmail.com>
2017-12-11 11:30:47 +0000

    226. Invert Binary Tree.

---
+++ problem/p226.go
@@ -0,0 +1,17 @@
+package problem
+
+import (
+	. "leetcode-go/util"
+)
+
+// O(n) time. O(log(n)) space. Recursive DFS.
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
